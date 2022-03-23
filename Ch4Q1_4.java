ea7574fac6fc03be8b365f0c3e22250ceb498c07
DESKTOP-GMDH8TL\luvma <kamisamatachi@naver.com>
2022-03-23 22:36:36 +0900

    다음 계산 결과를 출력하는 프로그램을 작성하되, 덧셈 연산의 횟수를 최소화하여 작성해보자. 3 + 6          3 + 6 + 9          3+ 6 + 9 + 12

---
+++ Ch4Q1_4.java
@@ -0,0 +1,12 @@
+class Ch4Q1_4 {
+		public static void main(String[] args) {
+				int num = 3 + 6;
+				System.out.println(num);
+
+				num += 9; // num = num + 9;
+				System.out.println(num);
+
+				num += 12; // num = num + 12;
+				System.out.println(num);
+	}
+}
